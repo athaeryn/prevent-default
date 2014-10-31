b528ca245123f7e6bcc25913de15d5f1ab355427
Mike Anderson <athaeryn@users.noreply.github.com>
2014-10-30 19:49:30 -0500

    Update prevent-default.js

--- prevent-default.js
+++ prevent-default.js
@@ -1,6 +1,6 @@
 module.exports = function preventDefault(listener) {
   return function(ev) {
     ev.preventDefault();
-    return listener.apply(this, arguments) === true;
+    return listener.apply(null, arguments);
   };
-};
+}
